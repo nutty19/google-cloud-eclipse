86f64fb0197db19fa5e3a8e723dcc1f63c5076f6
nbashirbello <nbashirbello@google.com>
2016-07-26 11:52:33 -0400

    Uninstalling an App Engine facet from a project removes all the App Engine runtime types from the list of targeted runtimes of that project

--- plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/FacetUninstallDelegate.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/FacetUninstallDelegate.java
@@ -1,14 +1,23 @@
 package com.google.cloud.tools.eclipse.appengine.facets;
 
+import java.util.Set;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jst.server.core.FacetUtil;
 import org.eclipse.wst.common.project.facet.core.IDelegate;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
-
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.eclipse.wst.common.project.facet.core.runtime.IRuntime;
 import com.google.cloud.tools.eclipse.util.MavenUtils;
 
 public class FacetUninstallDelegate implements IDelegate {
@@ -17,7 +26,9 @@ public class FacetUninstallDelegate implements IDelegate {
   public void execute(IProject project, IProjectFacetVersion version, Object config,
       IProgressMonitor monitor) throws CoreException {
     if (!MavenUtils.hasMavenNature(project)) { // Maven handles classpath in maven projects.
-      updateClasspath(project, monitor);
+      SubMonitor subMonitor = SubMonitor.convert(monitor, 100);
+      updateClasspath(project, subMonitor.newChild(50));
+      uninstallAppEngineRuntime(project, subMonitor.newChild(50));
     }
 
   }
@@ -40,5 +51,35 @@ public class FacetUninstallDelegate implements IDelegate {
     if(isAppEngineSdkPresent) {
       javaProject.setRawClasspath(newClasspath, monitor);
     }
+  }
+
+  /**
+   * Removes all the App Engine server runtimes from the list of targeted runtimes for
+   * <code>project</code>.
+   */
+  private void uninstallAppEngineRuntime(final IProject project, IProgressMonitor monitor) {
+    Job uninstallJob = new Job("") {
+
+      @Override
+      protected IStatus run(IProgressMonitor monitor) {
+        IFacetedProject facetedProject;
+        try {
+          facetedProject = ProjectFacetsManager.create(project);
+          Set<IRuntime> targetedRuntimes = facetedProject.getTargetedRuntimes();
+
+          for (IRuntime aRuntime : targetedRuntimes) {
+            org.eclipse.wst.server.core.IRuntime runtime2 = FacetUtil.getRuntime(aRuntime);
+            if (runtime2.getRuntimeType().getId().equals(AppEngineStandardFacet.DEFAULT_RUNTIME_ID)) {
+              facetedProject.removeTargetedRuntime(aRuntime, monitor);
+            }
+          }
+        } catch (CoreException e) {
+          return e.getStatus();
+        }
+        return Status.OK_STATUS;
+      }
+    };
+    uninstallJob.schedule();
+
   }
 }
---
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/RuntimeChangeListener.java
@@ -0,0 +1,73 @@
+package com.google.cloud.tools.eclipse.appengine.facets;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.events.IFacetedProjectEvent;
+import org.eclipse.wst.common.project.facet.core.events.IFacetedProjectListener;
+import org.eclipse.wst.common.project.facet.core.events.IPrimaryRuntimeChangedEvent;
+import org.eclipse.wst.common.project.facet.core.runtime.IRuntime;
+import org.eclipse.wst.common.project.facet.core.runtime.IRuntimeComponent;
+
+
+public class RuntimeChangeListener implements IFacetedProjectListener {
+
+  @Override
+  public void handleEvent(IFacetedProjectEvent event) {
+    if (event.getType() != IFacetedProjectEvent.Type.PRIMARY_RUNTIME_CHANGED) {
+      return;
+    }
+    
+    // If the App Engine runtime has been added as the primary runtime
+    // add the App Engine facet to the project
+    final IPrimaryRuntimeChangedEvent runtimeChangeEvent = (IPrimaryRuntimeChangedEvent)event;
+    IRuntime newRuntime = runtimeChangeEvent.getNewPrimaryRuntime();
+    if (newRuntime == null) {
+      return;
+    }
+
+    List<IRuntimeComponent> runtimeComponents = newRuntime.getRuntimeComponents();
+    for (IRuntimeComponent comp : runtimeComponents) {
+      if (comp.getRuntimeComponentType().getId().equals("com.google.appengine.runtime")) {        
+        // Add the App Engine facet
+        Job addFacetJob = new Job("Add App Engine facet...") {
+
+          @Override
+          protected IStatus run(IProgressMonitor monitor) {
+            IFacetedProject project = runtimeChangeEvent.getProject();
+            IStatus installStatus = Status.OK_STATUS;
+            
+            try {
+              FacetInstallDelegate.installAppEngineFacet(project, monitor);
+              return installStatus;
+            } catch (CoreException e) {
+              // Displays missing constraints that prevented facet installation
+              installStatus = e.getStatus();
+            }
+            
+            // Remove App Engine as primary runtime
+            try {
+              project.removeTargetedRuntime(runtimeChangeEvent.getNewPrimaryRuntime(), monitor);
+              return installStatus;
+            } catch (CoreException e) {
+              MultiStatus multi = (MultiStatus) installStatus;
+              multi.merge(e.getStatus());
+              return multi;
+            }
+          }
+          
+        };
+        addFacetJob.schedule();
+        
+      }
+    }
+    
+  }
+
+}
