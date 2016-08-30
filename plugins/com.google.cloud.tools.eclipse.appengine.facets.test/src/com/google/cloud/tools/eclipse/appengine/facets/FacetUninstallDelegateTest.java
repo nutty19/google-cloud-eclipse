c3a0f241ca4e72ded1ba8c4316b6df7f7d042761
nbashirbello <nbashirbello@google.com>
2016-08-30 10:35:19 -0400

    Adding FacetUninstallDelegateTest.java

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets.test/src/com/google/cloud/tools/eclipse/appengine/facets/FacetUninstallDelegateTest.java
@@ -0,0 +1,78 @@
+package com.google.cloud.tools.eclipse.appengine.facets;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.maven.model.Dependency;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FacetUninstallDelegateTest {
+  @Test
+  public void testCreateMavenProjectDependecies_nonAppEngineInitialDependency() {
+    Dependency nonAppEngineDependency = new Dependency();
+    nonAppEngineDependency.setGroupId("groupId");
+    nonAppEngineDependency.setArtifactId("artifactId");
+    nonAppEngineDependency.setVersion("version");
+    nonAppEngineDependency.setScope("scope");
+
+    List<Dependency> intialDependencies = new ArrayList<Dependency>();
+    intialDependencies.add(nonAppEngineDependency);
+
+    List<Dependency> finalDependencies = FacetUninstallDelegate.createMavenProjectDependecies(intialDependencies);
+    Assert.assertEquals(1, finalDependencies.size());
+  }
+
+  @Test
+  public void testCreateMavenProjectDependecies_appEngineInitialDependency() {
+    Dependency appEngineApiStubsDependency = new Dependency();
+    appEngineApiStubsDependency.setGroupId("com.google.appengine");
+    appEngineApiStubsDependency.setArtifactId("appengine-api-stubs");
+    appEngineApiStubsDependency.setVersion("${appengine.version}");
+    appEngineApiStubsDependency.setScope("test");
+
+    List<Dependency> intialDependencies = new ArrayList<Dependency>();
+    intialDependencies.add(appEngineApiStubsDependency);
+
+    List<Dependency> finalDependencies = FacetUninstallDelegate.createMavenProjectDependecies(intialDependencies);
+    Assert.assertTrue(finalDependencies.isEmpty());
+  }
+
+  @Test
+  public void testCreateMavenProjectDependecies_noInitialDependency() {
+    List<Dependency> initialDependencies = new ArrayList<Dependency>();
+    List<Dependency> finalDependencies1 = FacetUninstallDelegate.createMavenProjectDependecies(initialDependencies);
+    List<Dependency> finalDependencies2 = FacetUninstallDelegate.createMavenProjectDependecies(null);
+
+    Assert.assertTrue(finalDependencies1.isEmpty());
+    Assert.assertTrue(finalDependencies2.isEmpty());
+  }
+  
+  @Test
+  public void testUpdatePomProperties_nonAppEngineInitialPropertery() {
+    Properties properties = new Properties();
+    properties.setProperty("a", "b");
+
+    FacetUninstallDelegate.updatePomProperties(properties);
+    Assert.assertEquals(1, properties.size());
+    Assert.assertTrue(properties.containsKey("a"));
+  }
+  
+  @Test
+  public void testUpdatePomProperties_appEngineInitialPropertery() {
+    Properties properties = new Properties();
+    properties.setProperty("app.version", "1");
+
+    FacetUninstallDelegate.updatePomProperties(properties);
+    Assert.assertTrue(properties.isEmpty());
+  }
+  
+  @Test
+  public void testUpdatePomProperties_noInitialPropertery() {
+    Properties properties = new Properties();
+
+    FacetUninstallDelegate.updatePomProperties(properties);
+    Assert.assertTrue(properties.isEmpty());
+  }
+}
