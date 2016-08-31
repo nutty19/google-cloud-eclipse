245d759e02206f85532fd8f42de8dca52d5557e1
nbashirbello <nbashirbello@google.com>
2016-08-31 11:15:29 -0400

    Change default gcloud plugin version to "2.0.9.121.v20160815"

--- plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineStandardFacet.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineStandardFacet.java
@@ -54,7 +54,7 @@ public class AppEngineStandardFacet {
   static final String DEFAULT_RUNTIME_ID = "com.google.cloud.tools.eclipse.appengine.standard.runtime";
   static final String DEFAULT_RUNTIME_NAME = "App Engine Standard";
   public static final String DEFAULT_APPENGINE_SDK_VERSION = "1.9.42";
-  public static final String DEFAULT_GCLOUD_PLUGIN_VERSION = "2.0.9.106.v20160420";
+  public static final String DEFAULT_GCLOUD_PLUGIN_VERSION = "2.0.9.121.v20160815";
 
   /**
    * Returns true if project has the App Engine Standard facet and false otherwise.
