2a29aa99ee6f950cd7a601914d3bf3b4c64a140f
nbashirbello <nbashirbello@google.com>
2016-10-04 09:41:33 -0400

    Added tests for AppEngineFlexFacet#hasAppEngineFacet

--- plugins/com.google.cloud.tools.eclipse.appengine.facets.test/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineFlexFacetTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets.test/src/com/google/cloud/tools/eclipse/appengine/facets/AppEngineFlexFacetTest.java
@@ -1,13 +1,39 @@
 package com.google.cloud.tools.eclipse.appengine.facets;
 
+import static org.mockito.Mockito.when;
+
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AppEngineFlexFacetTest {
+  @Mock private IFacetedProject facetedProject;
+
   @Test
   public void testFlexFacetExists() {
     Assert.assertTrue(
         ProjectFacetsManager.isProjectFacetDefined("com.google.cloud.tools.eclipse.appengine.facets.flex"));
+  }
+
+  @Test
+  public void testHasAppEngineFacet_withFacet() {
+    IProjectFacet projectFacet = ProjectFacetsManager.getProjectFacet(AppEngineFlexFacet.ID);
+    when(facetedProject.hasProjectFacet(projectFacet)).thenReturn(true);
+
+    Assert.assertTrue(AppEngineFlexFacet.hasAppEngineFacet(facetedProject));
+  }
+
+  @Test
+  public void testHasAppEngineFacet_withoutFacet() {
+    IProjectFacet projectFacet = ProjectFacetsManager.getProjectFacet(AppEngineFlexFacet.ID);
+    when(facetedProject.hasProjectFacet(projectFacet)).thenReturn(false);
+
+    Assert.assertFalse(AppEngineFlexFacet.hasAppEngineFacet(facetedProject));
   }
 }
